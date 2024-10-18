ba47687a8be884feca5bc2ab40cdfd53771e1d02
Baktybeks <baktybeks@mail.ru>
2024-10-18 10:32:58 +0600

    lesson6

---
+++ lesson6/src/App.js
@@ -0,0 +1,19 @@
+import Main from './pages/Main';
+import UserPage from './pages/UserPage';
+import PokemonPage from './pages/pokemonPage/PokemonPage';
+import { Footer2 } from './components/Footer';
+import FormPage from './pages/formPage/FormPage';
+
+
+function App() {
+  return (
+    <div className="App">
+        {/*<Main/>*/}
+        {/*<UserPage/>*/}
+        {/*<PokemonPage/>*/}
+        <FormPage/>
+    </div>
+  );
+}
+
+export default App;
